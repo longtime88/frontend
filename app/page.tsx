46635e608278e19a5ffce3b3f9824bc962f32b98
max <myprogram@justmail.de>
2026-03-27 18:02:29 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,15 @@ import Image from "next/image";
 export default function Homepage() {
   return (
     <div>
-      <h1 className="text-2xl font-bold">Willkommen bei Luna&Clean</h1>
-
-    <div className="realtive w-full h-72 mt-4">
+      
+    <div className=" bg-white shadow flex items-center px-6 relative z-20 w-full h-[calc(100vh-5rem)]">
       <Image
         src="/images/Hintergrund.png"
           alt="Hintergrund"
           fill
-        className="object-cover"
+          className="object-cover"
+          loading="eager"
+          priority
         />
       </div>
     </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,8 @@ import Link from "next/link"
 
 export default function Header() {
   return (
-    <header className="w-full shadow px-6 py-2 flex flex-col gap-4 bg-blue-700">
-
+    
+        <header className="w-full  shadow px-6 py-2 flex flex-col gap-4 bg-blue-700">
       <div className="text-center">
         <h1 className="text-3xl font-bold text-white/80 select-none">
           Luna&Clean
