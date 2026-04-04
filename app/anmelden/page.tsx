04423f6ac43351a7ff5b3c691fc4de6e0c6a6605
max <myprogram@justmail.de>
2026-04-05 01:37:13 +0200

    first commit

--- app/anmelden/page.tsx
+++ app/anmelden/page.tsx
@@ -11,7 +11,7 @@ export default function Anmelden() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:8000/api/login", {
+      const res = await fetch("http://localhost:8080/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
